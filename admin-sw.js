70b45e22e1c5afe9bb8e4cd16358b3b043902158
ScolaritePSHSUP <scolaritepshsup@passy-st-honore.com>
2026-01-19 22:48:25 +0100

    Add files via upload

--- admin-sw.js
+++ admin-sw.js
@@ -1,39 +1,18 @@
-const CACHE_NAME = "psh-admin-v3"; // ⬅️ CHANGE LA VERSION À CHAQUE MODIF
+const CACHE_NAME = "psh-admin-v4";
 const BASE_PATH = "/PSH-SHUP-ADMIN";
 
-const ASSETS = [
-  `${BASE_PATH}/`,
-  `${BASE_PATH}/admin-login.html`,
-  `${BASE_PATH}/admin-dashboard.html`,
-  `${BASE_PATH}/style.css`,
-  `${BASE_PATH}/admin-manifest.json`,
-  `${BASE_PATH}/admin-sw.js`,
-  `${BASE_PATH}/icons/icon-192.png`,
-  `${BASE_PATH}/icons/icon-512.png`
-];
-
 // INSTALL
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
 // ACTIVATE (nettoyage anciens caches)
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
+self.addEventListener("activate", (event) => {
   self.clients.claim();
 });
 
 // FETCH
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then(res => res || fetch(event.request))
   );
