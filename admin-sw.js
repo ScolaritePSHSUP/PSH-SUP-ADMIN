00deb700593aeb30a9b7a8381c93b2e0ca404726
ScolaritePSHSUP <scolaritepshsup@passy-st-honore.com>
2026-01-19 22:55:35 +0100

    Add files via upload

--- admin-sw.js
+++ admin-sw.js
@@ -1,19 +1,18 @@
-const CACHE_NAME = "psh-admin-v4";
-const BASE_PATH = "/PSH-SHUP-ADMIN";
+const CACHE_NAME = "psh-admin-v5";
+const BASE = "/PSH-SHUP-ADMIN";
 
-// INSTALL
-self.addEventListener("install", (event) => {
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-// ACTIVATE (nettoyage anciens caches)
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", () => {
   self.clients.claim();
 });
 
-// FETCH
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    fetch(event.request).catch(() =>
+      caches.match(`${BASE}/index.html`)
+    )
   );
 });
